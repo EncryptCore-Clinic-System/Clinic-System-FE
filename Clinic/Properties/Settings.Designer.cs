8e8baa3e3977decd3cfa2bd85647d1f393f1459b
Mahmoud Elsalmy <mahmoud.sherif5@msa.edu.eg>
2025-09-01 00:46:49 +0300

    Add files via upload

--- Clinic/Properties/Settings.Designer.cs
+++ Clinic/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Clinic.Properties
+namespace ClinicSystem.Properties
 {
 
 
---
+++ Clinic/migrations/202508311934504_Meg.cs
@@ -0,0 +1,16 @@
+﻿namespace ClinicSystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Meg : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
