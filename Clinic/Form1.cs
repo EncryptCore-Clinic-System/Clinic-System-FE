3a3230654ad7970998e4841ba44d2a8fac11b711
lina <linawalid633@gmail.com>
2025-09-02 00:59:43 +0300

    welcome screen

--- Clinic/Form1.cs
+++ Clinic/Form1.cs
@@ -16,5 +16,30 @@ namespace Clinic
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+              
+                   }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
